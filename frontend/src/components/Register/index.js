3af73465e5352d37597c25585a5c79bbaa5c8efd
SNEHASIS DUTTA <162689269+SNEHASIS-CODEHUB04@users.noreply.github.com>
2025-09-09 21:12:59 +0530

    Update index.js

--- frontend/src/components/Register/index.js
+++ frontend/src/components/Register/index.js
@@ -15,7 +15,7 @@ const Register = () => {
       return;
     }
     try {
-      const response = await fetch('http://localhost:5000/api/users/register', {
+      const response = await fetch('https://collab-board-backend-13td.onrender.com/api/users/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
