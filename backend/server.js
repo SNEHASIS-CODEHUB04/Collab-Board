d7fefa0dbe83102d1dec9c58b4561c8d6f3ca517
SNEHASIS DUTTA <162689269+SNEHASIS-CODEHUB04@users.noreply.github.com>
2025-09-09 21:21:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ connectToDB();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:3000"], //"https://whiteboard-tutorial-eight.vercel.app"
+      origin: ["https://collab-board-frontendx.onrender.com"], 
       methods: ["GET", "POST"],
     },
   });
